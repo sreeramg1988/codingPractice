4408b6369231362c3ed2dab3bd19e7469fb7fa31
sreeramg1988 <sreeramg1988@gmail.com>
2020-04-11 22:37:54 +0530

    Update on the logic

--- src/main/java/FactorialOfANumber.java
+++ src/main/java/FactorialOfANumber.java
@@ -7,23 +7,18 @@ public class FactorialOfANumber {
     /*
     * This function is to find the factorial of an input number.
      */
-     public static void findFactorial(int number){
-         int result = 1;
-         for(int i = 1; i <= number; i++){
-             result = result * i;
-         }
-        System.out.println("The factorial of the number is: " + result);
+     public static int findFactorial(int number){
+         if(number <= 1)
+             return 1;
+         else
+             return number*findFactorial(number-1);
      }
 
     public static void main (String args[]){
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the number: ");
         inputNumber = scan.nextInt();
-        if(inputNumber == 0 || inputNumber < 0){
-            System.out.println("Please enter a positive, non-zero number");
-             }
-        System.out.println("Input number: " + inputNumber);
-        findFactorial(inputNumber);
+        System.out.println(findFactorial(inputNumber));
         scan.close();
     }
 }
