239f2f713bd370fc29bb988a7635f3200de4761d
sreeramg1988 <sreeramg1988@gmail.com>
2020-04-11 23:50:15 +0530

    Added: Program to check whether the given String is Palindrome or not

---
+++ src/main/java/CheckForPalindromeString.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class CheckForPalindromeString {
+
+    /**
+     * This methodis to check whether the entered String is Palindrome
+     * @param inputString
+     * @return true if the string is a palindrome
+     * @return false if the String is not a palindrome
+     */
+    public static boolean isPalindromeString(String inputString){
+        String resultString = "";
+        for(int i = inputString.length()-1; i >= 0; i--){
+            resultString = resultString + inputString.charAt(i);
+        }
+        if(inputString.equalsIgnoreCase(resultString)){
+         return  true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static void main (String[] args){
+        Scanner scan = new Scanner(System.in);
+        String inputString = "";
+        System.out.println("Enter the String to be checked: ");
+        inputString = scan.next();
+        if(isPalindromeString(inputString)){
+            System.out.println("The String " + inputString + " is a Palindrome");
+        }
+        else{
+            System.out.println("The String " + inputString + " is not a Palindrome");
+        }
+        scan.close();
+    }
+
+}
