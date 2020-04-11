447421507e2912b195160b6d10d88b15834f3eee
sreeramg1988 <sreeramg1988@gmail.com>
2020-04-11 23:18:24 +0530

    Added : CheckPalindromeNumber.java

---
+++ src/main/java/CheckPalindromeNumber.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class CheckPalindromeNumber {
+
+    /*
+    * This boolean function will check and return true if the number is Palindrome
+     */
+    public static boolean isPalindromeNumber(int number){
+        int result = 0 , remainder, originalNumber;
+        originalNumber = number;
+         while(number > 0){
+             remainder = number % 10;
+             result = remainder + (result *10);
+             number = number /10 ;
+         }
+         if(originalNumber == result)
+             return true;
+         else
+             return false;
+    }
+
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int inputNumber;
+        System.out.println("Enter the number to check: ");
+        inputNumber = scan.nextInt();
+        if(isPalindromeNumber(inputNumber))
+            System.out.println("The entered number " + inputNumber + " is Palindrome");
+        else
+            System.out.println("The entered number " + inputNumber + " is not Palindrome");
+
+    }
+}
