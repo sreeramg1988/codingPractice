f7abf91355373eee1a849001c2d7d1d2ba805a6e
sreeramg1988 <sreeramg1988@gmail.com>
2020-09-10 01:10:43 +0530

    RegEx to identify whether the String contains any of the special chars in Keyboard

---
+++ src/main/java/FindSpecialCharactersInString.java
@@ -0,0 +1,17 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.CASE_INSENSITIVE;
+
+public class FindSpecialCharactersInString {
+
+    public static void main(String [] args){
+        String content = "Following example illustrates how to find a sequence of special characters like !@2019 together ";
+        Pattern pattern = Pattern.compile("\\p{Punct}{2,}");
+        Matcher matcher = pattern.matcher(content);
+        if (matcher.find()){
+           System.out.println("The special characters in the String are: " + matcher.group());
+        }
+    }
+
+}
