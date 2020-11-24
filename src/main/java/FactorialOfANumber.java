8bf451b6eca89c864e6c3a7dae2a7ad89782152b
sreeramg1988 <sreeramg1988@gmail.com>
2020-11-24 22:31:40 +0530

    Improvisation on finding the factorial of small numbers Test cases covered 1. Input is an integer 2. Input is non-negative 3. If input is 0 then the factorial should be 1

--- src/main/java/FactorialOfANumber.java
+++ src/main/java/FactorialOfANumber.java
@@ -2,23 +2,36 @@ import java.util.Scanner;
 
 public class FactorialOfANumber {
 
-    public static int inputNumber;
+
 
     /*
     * This function is to find the factorial of an input number.
      */
      public static int findFactorial(int number){
-         if(number <= 1)
+         if(number == 0)
              return 1;
          else
              return number*findFactorial(number-1);
      }
 
     public static void main (String args[]){
+        int inputNumber ;
         Scanner scan = new Scanner(System.in);
+
         System.out.println("Enter the number: ");
-        inputNumber = scan.nextInt();
-        System.out.println(findFactorial(inputNumber));
-        scan.close();
+        //check whether the input is an integer
+        if(scan.hasNext()){
+            inputNumber = scan.nextInt();
+            //check whether the input is non-negative using signum function
+            if(Integer.signum(inputNumber) != -1) {
+                System.out.println(findFactorial(inputNumber));
+                scan.close();
+            } else{
+                System.out.println("Enter a non-negative integer value");
+            }
+
+        }else{
+            throw new NumberFormatException();
+        }
     }
 }
