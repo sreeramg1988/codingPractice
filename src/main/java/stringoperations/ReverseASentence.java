72497eb3b1190dacf21ebb478f03a4a7a9871180
sreeramg1988 <sreeramg1988@gmail.com>
2020-12-03 21:54:42 +0530

    Program to Reverse a sentence

---
+++ src/main/java/stringoperations/ReverseASentence.java
@@ -0,0 +1,30 @@
+package stringoperations;
+
+import java.util.Scanner;
+
+public class ReverseASentence {
+
+    
+    static String reverseSentence(String inputString){
+        String outputString = "";
+
+        String [] wordsInString = inputString.split("\\s");
+        for(int i = wordsInString.length - 1; i >= 0 ; i--){
+            outputString = outputString + wordsInString[i] + " ";
+        }
+
+        return outputString;
+    }
+
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the sentence to reverse.");
+        if(scan.hasNext()) {
+            String inputString = scan.nextLine();
+            System.out.println("The reversed sentence is: " + reverseSentence(inputString));
+        }else{
+            System.out.println("Enter a valid string");
+        }
+    }
+}
